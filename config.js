bf9e3e5dff0a07503839f323cd31487b664764f9
SeptimusVII <vansteene.quentin@gmail.com>
2025-10-28 18:39:08 +0100

    add config

--- config.js
+++ config.js
@@ -1,1 +1,22 @@
-module.exports = {};
+module.exports = {
+	'colors' : {
+		'blueLogo':     '#0d7cfe',
+		'blueLogoFont': '#0032b2',
+		'redLogo':      '#ff0303',
+		'yellowLogo':   '#ffff00',
+		'green':        '#5cb85c',
+		'orange':       '#DC6053',
+	},
+
+	'primary': 'colors(blueLogo)',
+	'secondary': 'adjust-color(primary,$hue: 180deg)',
+
+	'success': 'colors(green)',
+	'info': 'colors(blue)',
+	'warning': 'colors(yellow)',
+	'error': 'colors(red)',
+
+	'link': {
+		'font-color': 'colors(blueLogoFont)',
+	},
+};
